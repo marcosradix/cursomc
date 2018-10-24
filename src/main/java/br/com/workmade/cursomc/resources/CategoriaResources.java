22716366a4c3b11aea19e8e7be47bd96352f535e
marcosradix <marcosradix@gmail.com>
2018-10-24 16:33:44 -0300

    adicionado classe de domain

--- src/main/java/br/com/workmade/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/com/workmade/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,27 @@
 package br.com.workmade.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.workmade.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value={"/", "/cursomc"})
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET, value="/categorias")
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Infomática");
+		
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 
